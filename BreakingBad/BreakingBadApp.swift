cdce93527deafb771a82235fba858a1e02c99a48
cohenzuri <57574358+cohenzuri@users.noreply.github.com>
2023-01-23 11:58:19 +0200

    Initial Commit

---
+++ BreakingBad/BreakingBadApp.swift
@@ -0,0 +1,17 @@
+//
+//  BreakingBadApp.swift
+//  BreakingBad
+//
+//  Created by zuri cohen on 1/23/23.
+//
+
+import SwiftUI
+
+@main
+struct BreakingBadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
